33ddcd594daa61618d249a291d2465150274858a
Jure Golob <95366061+IamTV50@users.noreply.github.com>
2024-09-03 13:41:37 +0200

    added BrandsDropdown component

---
+++ frontend/src/components/BrandsDropdown.vue
@@ -0,0 +1,25 @@
+<script setup>
+import Select from 'primevue/select';
+
+import {onMounted} from "vue";
+import {useFetch} from "@/utils/useFetch.js";
+
+const brands = defineModel('brandsList');
+const selectedBrand = defineModel('selectedBrand');
+
+onMounted(async () => {
+    const { data, loading, loadingError } = await useFetch('http://localhost:3000/ram/brands');
+
+    brands.value = data.value;
+});
+</script>
+
+<template>
+    <Select v-model="selectedBrand"
+            :options="brands"
+            optionLabel="name"
+            :placeholder="selectedBrand"
+            :invalid="selectedBrand === null" />
+</template>
+
+<style scoped></style>
