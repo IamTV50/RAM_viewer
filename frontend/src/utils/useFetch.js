8d24e4f58df26b9a118aaab07139d9167bd940c4
Jure Golob <95366061+IamTV50@users.noreply.github.com>
2024-09-03 15:21:49 +0200

    added options to useFetch

--- frontend/src/utils/useFetch.js
+++ frontend/src/utils/useFetch.js
@@ -1,12 +1,12 @@
 import { ref } from 'vue';
 
-export const useFetch = async (url) => {
+export const useFetch = async (url, options = null) => {
     const data = ref([]);
     const loading = ref(true);
     const loadingError = ref(false);
 
     try{
-        const res = await fetch(url);
+        const res = await fetch(url, options);
 
         if(!res.ok){ throw new Error('Failed to fetch'); }
 
